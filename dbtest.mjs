8f774503020e419364467ca18211c78e4c5c8c6c
SV-1411 <shivansh1411@gmail.com>
2026-06-18 18:35:41 +0530

    Remove throwaway DB connectivity test scripts

--- dbtest.mjs
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-try {
-  const n = await p.user.count();
-  console.log('OK users=', n);
-} catch (e) {
-  console.log('FAIL', e.message.split('\n').slice(0,3).join(' | '));
-} finally { await p.$disconnect(); }
--- dbtest2.mjs
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-try {
-  const n = await p.user.count();
-  console.log('OK users=', n);
-} catch (e) {
-  console.log('CODE', e.errorCode);
-  console.log('MSG', e.message);
-} finally { await p.$disconnect(); }
